a1e11b6bfc731288ad2cf96f9fc0f2d93eda0eb9
charlie1099 <cdcharlesworth@gmail.com>
2022-10-02 18:12:07 -0400

    finished exercises

--- ch2/ch2exercises.js
+++ ch2/ch2exercises.js
@@ -22,7 +22,7 @@ both shoould show
 #######
 */
 
-//Fizz Buzz
+// Fizz Buzz
 for(number = 1; number <= 100; number++) {
     let line = ""
     if(number % 3 === 0){
@@ -31,4 +31,36 @@ for(number = 1; number <= 100; number++) {
         line += "Bizz"
     }
     console.log(line || number)
-}// you could just do two if statments insted of the else if and one line them
+}
+// you could just do two if statments insted of the else if and one line them
+
+//Chessboard
+let size = 8
+let board = ""
+
+for(y = 0; y < size; y++) {
+    // the outer loop will complet onence but the inner will runn 8 times 
+    // inner hadels what is printed on the line
+    for(x = 0; x < size; x++) {
+        if((x + y) % 2 === 0) {
+            board += " "
+        } else {
+            board += "#"
+        }
+    }
+    // this will cuase the pattern that we need by useing an escape
+    board += "\n"
+}
+console.log(board)
+
+/* what it looks like
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+
+*/
