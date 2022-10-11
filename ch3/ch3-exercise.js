2b5cfbecdaec17e11a55d1f3777419d8cbf9f02b
charlie1099 <cdcharlesworth@gmail.com>
2022-10-10 20:42:07 -0400

    finished ch3

--- ch3/ch3-exercise.js
+++ ch3/ch3-exercise.js
@@ -31,9 +31,22 @@ console.log(isEven(-1));
 // → false
 
 
-function countBs (input) {
-    let inputString = String(input)
-    for (let inputString = ""; inputString.length > ""; inputString++) {
-        
+function countChar(string, ch) {
+    let counted = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] == ch) {
+            counted += 1;
+        }
+        // console.log(i)
+        console.log(string[i])
     }
+    return counted;
 }
+
+function countBs(string) {
+    return countChar(string, "B");
+}
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
