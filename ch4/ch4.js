fc0813b3d0bf56ca44b32c95623155d29498afeb
charlie1099 <cdcharlesworth@gmail.com>
2022-10-19 21:37:19 -0400

    almost done with ch4

--- ch4/ch4.js
+++ ch4/ch4.js
@@ -82,6 +82,115 @@ console.log(day1.wolf)
 Arrys are a kinda object that specialized in storing a sequences of thing and they can store objects in them and be known as an array of objects
 
 Mutability
+Objects can be modified however when it comes to other values such as numbers, strings, and Booleans they can not be so they are 
+whats called immutable it is impossible to change values of thoes types.
+
+Objects work in away so you can change their properties causing a single object value to change.
+
+also there is a diffrence in having two references to the same object and having two objects that contain the same properties
+for example
+*/
+let object1 = {value: 10};
+let object2 = object1;
+let object3 = {value: 10};
+
+console.log(object1 == object2)
+// this will show true becuse obj2 value is set to equl obj1
+console.log(object1 == object3)
+// this will show as false becuse these are two diffrent objects even if they have the same value
+
+object1.value = 15;
+console.log(object2.value)
+// the value will show 15 becuse object1s value was updated so object2 will update in tandum
+
+console.log(object3)
+// the value will still be 10 becuse it was never updated
+
+/*
+When you use a const binding tho the object can not be changed becuse it is a const variable
+you can update them like so 
+*/
+const score = {visitors: 0, home: 0};
+//This is ok
+score.visitors = 1 
+
+// This is not
+//score = {visitors: 0, home: 0}
+
+/*
+when you compare objects in JavaScript with the == operator it will compare by the identity it will show true if they are the same
+value. But it will show false even if they have the same identical properties there is no deep comparison in JavaScript which compares
+object by contents. you would have to write that yourself.
+*/
+
+let journal = []
+function addEntry(events, squirrel) {
+    journal.push({events, squirrel})
+}
+// this is allowed the poperty name and value are the same thing 
+
+/*
+Looping over objects
+
+In javaScript you can loop over an array with a for loop with like this 
+
+for (let i = 0; i < JOURNAL.length; i++) {
+    let entry = JOURNAL[i]
+    // do somthing...
+}
+
+but with objects it is diffrent you would do this like so
+for (let entry of JOURNAL) {
+    consle.log(`${entry.events.length} events`);
+}
+the of key word will loop over the elementd of the value given after of. This can work for other data structures.
+
+Further Arrayology
+In arrays to add or remove things on the end you would use push to add and pop to remove.
+
+Adding and removing from the start you would use unshift and shift
+*/
+
+let todoList = []
+
+function remember(task) {
+    todoList.push(task);
+}
+// removes and returns the first element
+function getTask() {
+    return todoList.shift();
+}
+//inserts the element to the front of the array
+function rememberUrgently(task) {
+    todoList.unshift(task);
+}
+
+// you can also get a specific tasks index how you would do that is indexOf(the number of the index you want here)
+console.log([1, 2, 3, 4].indexOf(2))
+//1
+//you can start the search from the end with doing .lastIndexOf method
+/*
+these methods do take a second optional arg that will let you chose where to start the search from a index
+
+Another useful array method is the .slice it will take off all of the elements after the start index and end index
+and will return a new array
+
+the start index is included 
+but the end index is not
 
 */
+console.log([1, 2, 3, 4].slice(2, 4))
+//[2, 4]
+console.log([1, 2, 3, 4].slice(2))
+//[2, 3, 4]
+
+// Intrestingly youi can glue arrys togeather by useing the cancat method like so
+function remove(array, index) {
+    return array.slice(0, index)
+        .concat(array.slice(index + 1));
+}
+console.log(remove(["a", "b", "c", "d", "e"], 2));
+// ["a", "b", "d", "e"]
+
+
 
