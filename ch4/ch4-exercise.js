7822696370cbebe4a904c9176d3122e27e47c10f
charlie1099 <cdcharlesworth@gmail.com>
2022-11-11 18:18:26 -0500

    finished ch4

--- ch4/ch4-exercise.js
+++ ch4/ch4-exercise.js
@@ -9,28 +9,26 @@ as a bouns modify the rang function to take a step/anamout to increment by for t
 
 */
 
-
-
-function range (start, end, step = start < end ? 1 : -1) {
+function range(start, end, step = start < end ? 1 : -1) {
   let array = [];
-    
-  if (step > 0) {
-    for(let i = start; i <= end; i += step) array.push(i)
-  } else {
-    for(let i = start; i >= end; i += step) array.push(i)
-  }
-  
-  return array;
-};
 
-function sum (array) {
-let total = 0;
-for(let value of array) {
-    total += value
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) array.push(i);
+  } else {
+    for (let i = start; i >= end; i += step) array.push(i);
+  }
+
+  return array;
 }
-return total
+
+function sum(array) {
+  let total = 0;
+  for (let value of array) {
+    total += value;
+  }
+  return total;
 }
-console.log(range(1, 10))
+console.log(range(1, 10));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
@@ -53,12 +51,11 @@ neither may use the standerd reverse method.
 function reverseArray(array) {
   let array2 = [];
 
-  for(let i = array.length -1; i >= 0; i-- ) {
-    array2.push(array[i])
+  for (let i = array.length - 1; i >= 0; i--) {
+    array2.push(array[i]);
   }
-return array2
+  return array2;
 }
-
 
 //this does not use memory also it will change it on the globle scop
 function reverseArrayInPlace(array) {
@@ -76,3 +73,97 @@ let arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
 // → [5, 4, 3, 2, 1]
+
+/* 
+A List
+Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument
+
+Also write a listToArray function that produces an array from a list THEN add a helper function prepend
+which will takes an element and a list creates a new lis that adds the element to the front of the inputlist
+
+then write nth which takes a list and a number and returns the elemint at the given position/index
+*/
+
+let list = {
+  value: 1,
+  rest: {
+    value: 2,
+    rest: {
+      value: 3,
+      rest: null,
+    },
+  },
+};
+console.log(list);
+
+function arrayToList(array) {
+ let list = null
+
+  for(let i = array.length - 1; i >= 0; i--) {
+    list = {value: array[i], rest: list}
+    console.log(list)
+  }
+  console.log(list)
+return list
+}
+
+function listToArray(list) {
+   let array = []
+
+   for(let node = list; node; node = node.rest) {
+      array.push(node.value)
+   }
+return array
+}
+
+function prepend(value, list) {
+  return {value, rest: list};
+}
+
+function nth(list, n) {
+  if (!list) return undefined;
+  else if (n == 0) return list.value;
+  else return nth(list.rest, n - 1)
+}
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
+
+/* 
+Deep Comparison
+The == operator compaires objects by identity. but sometimws you'd prefer to compaire the values of their actual properties
+write a function called deepComparison that will take two values and return true only if they are the same value or are obj
+
+*/
+
+function deepEqual(a, b) {
+  if (a === b) return true;
+  
+  if (a == null || typeof a != "object" ||
+      b == null || typeof b != "object") return false;
+
+  let keysA = Object.keys(a), keysB = Object.keys(b);
+
+  if (keysA.length != keysB.length) return false;
+
+  for (let key of keysA) {
+    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+  }
+
+  return true;
+}
+
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
+
